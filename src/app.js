3292c86b91dd07a1e64957f2e6cf9cfed04d9f1e
wjdwwidz <wjdwwidz@gmail.com>
2023-11-15 15:16:02 +0900

    feat: Add server listening code and refactor

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger-output.json');
-const mongoose = require('mongoose');
+const cors = require('cors');
 require("dotenv").config();
-// const cors = require('cors');
 
-const taskRouter = require('./routers/TaskRouter');
-// const communityRouter = require('./routers/communityRouter');
 const app = express(); 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mongoose
     .connect (process.env.MONGO_DB_URL)
@@ -22,37 +16,19 @@ mongoose
         console.error('MongoDB 연결 실패: ', error);
     });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
 
 app.get('/', (req, res) => {
-    res.send('hello express');
+    res.send('Hello express !');
 });
 
-app.get('/community', (req, res) => {
-    //#swagger.summary = 산책 매칭글 리스트 페이지'
-    //#swagger.tags = ['community']
-    /* #swagger.parameters['community_name'] = {
-        in: 'header',                            
-        description: 'params desc',                                 
-        type: 'boolean',                         
-} */
-});
 
-// app.use("/", communityRouter);
-
-app.get("/ping", (req, res) => {
-    //#swagger.summary = '어디에 무슨 설명을 쓰겠다는 거에요 이거에요 (summary)?'
-    //#swagger.tags = ['ping-pong']
-    //#swagger.description = '이거에요? (desc)'
-    /* #swagger.parameters['category_name'] = {
-        in: 'header',                            
-        description: 'params desc',                                 
-        type: 'boolean',                         
-} */
-    res.send("pong");
-});
-
-app.use("/tasks", taskRouter);
-
-app.listen(3000);
+app.listen(process.env.PORT, () => {
+    console.log(`Express server starting on port ${process.env.PORT}`);
+  });
