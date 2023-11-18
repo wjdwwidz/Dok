6cf263030273b645286095ee3d846a588efc1f43
wjdwwidz <wjdwwidz@gmail.com>
2023-11-18 19:41:43 +0900

    refactor: Optimize code

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,25 +1,26 @@
+const CustomError = require('../errors/customError');
 const User = require('../models/user/user');
 const PasswordEncoder = require('../utils/passwordEncoder');
 
 async function createUser(userRequest) {
+  const encryptedPassword = await PasswordEncoder.hash(
+    userRequest.getPassword(),
+  );
+
+  const user = new User({
+    //mongoose dao 역할 이자 모델
+    userId: userRequest.getUserId(),
+    password: encryptedPassword,
+    name: userRequest.getName(),
+    nickname: userRequest.getNickname(),
+  });
+
   try {
-    const encryptedPassword = await PasswordEncoder.hash(
-      userRequest.getPassword(),
-    );
-
-    const user = new User({
-      //mongoose dao 역할 이자 모델
-      userId: userRequest.getUserId(),
-      password: encryptedPassword,
-      name: userRequest.getName(),
-      nickname: userRequest.getNickname(),
-    });
-
     await user.save();
     return user;
   } catch (error) {
     console.error(error);
-    throw error;
+    throw new CustomError(error.message, 500);
   }
 }
 
