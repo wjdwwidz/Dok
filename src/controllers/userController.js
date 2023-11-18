5902e3248a7107d48c50aa00c76802588d60e893
wjdwwidz <wjdwwidz@gmail.com>
2023-11-18 22:04:08 +0900

    refactor: control about unkown error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const userService = require('../services/userService');
 const UserRequest = require('../dtos/userRequest');
+const falsey = require('falsey');
 
 async function signUp(req, res) {
   const { userId, password, name, nickname } = req.body;
@@ -10,6 +11,9 @@ async function signUp(req, res) {
     const user = await userService.createUser(userRequest);
     res.status(201).json(user);
   } catch (error) {
+    if (falsey(error.getStatusCode)) {
+      res.status(500).json(error.message);
+    }
     res.status(error.getStatusCode()).json(error.getMessage());
   }
 }
