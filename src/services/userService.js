7760bb746e5b00fae2786b64642ffeec676cbce0
wjdwwidz <wjdwwidz@gmail.com>
2023-11-18 18:45:12 +0900

    feat: Apply password encryption

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,16 @@
 const User = require('../models/user/user');
+const PasswordEncoder = require('../utils/passwordEncoder');
 
 async function createUser(userRequest) {
   try {
+    const encryptedPassword = await PasswordEncoder.hash(
+      userRequest.getPassword(),
+    );
+
     const user = new User({
       //mongoose dao 역할 이자 모델
       userId: userRequest.getUserId(),
-      password: userRequest.getPassword(),
+      password: encryptedPassword,
       name: userRequest.getName(),
       nickname: userRequest.getNickname(),
     });
