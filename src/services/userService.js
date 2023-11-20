0b13d964df283e9206d491f739fa0bcf33a97472
wjdwwidz <wjdwwidz@gmail.com>
2023-11-20 22:41:02 +0900

    feat: Add JWT token generation

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,7 @@ const UnauthenticationError = require('../errors/unauthenticationError');
 const NotFoundError = require('../errors/notFoundError');
 const User = require('../models/user/user');
 const PasswordEncoder = require('../utils/passwordEncoder');
+const JwtUtil = require('../utils/jwtUtil');
 const falsey = require('falsey');
 
 async function createUser(userCreateRequest) {
@@ -21,10 +22,16 @@ async function createUser(userCreateRequest) {
   return user;
 }
 
-async function signIn(userId, password) {
+async function signIn(res, userSignInRequest) {
+  const userId = userSignInRequest.getUserId();
+  const password = userSignInRequest.getPassword();
+
   const user = await User.findOne({ userId: userId });
   if (falsey(user)) {
     throw new NotFoundError(`존재하지 않는 아이디입니다. inputId: ${userId}`);
+    // //다른 계층에서 error 처리하도록 return하지 않고 throw
+    // //error: statuscode는 전달할 수 없다
+    // throw new Error(`존재하지 않는 아이디입니다. inputId: ${userId}`);
   }
 
   const isMatch = await PasswordEncoder.compare(password, user.password);
@@ -33,6 +40,9 @@ async function signIn(userId, password) {
       `비밀번호가 일치하지 않습니다. inputPassword: ${password}`,
     );
   }
+  const jwtUtil = new JwtUtil();
+  const token = await jwtUtil.encode(user._id);
+  res.header('Bearer', `${token}`);
 
   return user;
 }
