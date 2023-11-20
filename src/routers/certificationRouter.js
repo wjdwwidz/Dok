e0e74322da20bb43d5f16f1010b3c46e810a1394
BangJunha <tkdlqjqkd@gmail.com>
2023-11-20 19:17:18 +0900

    fix: Fix certificationPostRouter path

--- src/routers/certificationRouter.js
+++ src/routers/certificationRouter.js
@@ -3,17 +3,18 @@ const {
   getCertificationPosts,
   getCertificationPostDetails,
   postCertificationPosts,
+  postCertificationPostReviews,
 } = require('../controllers/certificationPostController');
 
 const router = Router();
 
 // 인증글 생성
-router.post('/api/postCertificationPost', postCertificationPosts);
+router.post('/postCertificationPost', postCertificationPosts);
 // 전체 인증글 조회
-router.get('/api/getCertificationPost', getCertificationPosts);
+router.get('/getCertificationPost', getCertificationPosts);
 // 상세 인증글 조회
-router.get('/api/getCertificationPostDetails', getCertificationPostDetails);
+router.get('/getCertificationPostDetails', getCertificationPostDetails);
 // 리뷰 생성
-router.post('/');
+router.post('/postCertificationPostReview', postCertificationPostReviews);
 // 리뷰 수정
 module.exports = router;
