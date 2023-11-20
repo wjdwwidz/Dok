f9da2f946b5a73460d517e3a7abea5d1d7c47062
wjdwwidz <wjdwwidz@gmail.com>
2023-11-20 23:17:52 +0900

    feat: Add user signIn function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,11 @@
 const userService = require('../services/userService');
-const UserCreateRequest = require('../dtos/userCreateRequest');
+const UserCreateRequest = require('../dtos/users/userCreateRequest');
+const UserSignInRequest = require('../dtos/users/userSignInRequest');
 const falsey = require('falsey');
 
 async function signUp(req, res) {
   const { userId, password, name, nickname } = req.body;
-  //Validation required
   try {
-    // userRequest 생성 실패시 400에러
     const userRequest = new UserCreateRequest(userId, password, name, nickname);
     const user = await userService.createUser(userRequest);
     res.status(201).json(user);
@@ -21,14 +20,19 @@ async function signUp(req, res) {
 async function signIn(req, res) {
   const { userId, password } = req.body;
   try {
-    const user = await userService.signIn(userId, password);
-    res.status(200).json(user);
+    const userSignInRequest = new UserSignInRequest(userId, password);
+    const user = await userService.signIn(res, userSignInRequest);
+    res.status(201).json(user);
   } catch (error) {
+    // next(error);
+    // res.status(404).json(error.message);
+    console.log(error);
     if (falsey(error.getStatusCode)) {
       res.status(500).json(error.message); //우리가 모르는 에러는 500으로 던진다
     }
-    res.status(error.getStatusCode()).json(error.getMessage()); //커스텀에러 상속 받은 모든 에러
+    res.status(error.getStatusCode()).json(error.getMessage()); //커스텀에러 상속받은 모든 에러
   }
 }
 
 module.exports = { signUp, signIn };
+//error middleware 사용하여 더이상 코드 작성 하지 않을 정도로 정리해보기
