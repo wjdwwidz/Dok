6a486bfaef969f97b026c4a5cef1647766c10b8d
wjdwwidz <wjdwwidz@gmail.com>
2023-11-20 23:20:47 +0900

    chore: Add coment

--- src/errors/badReqestError.js
+++ src/errors/badReqestError.js
@@ -7,7 +7,9 @@ const CustomError = require('./customError');
 class BadRequestError extends CustomError {
   constructor(message) {
     super(message, 400);
-
+    //400이라는 값을 매번 전달해야하는 불편함
+    // => customError라는 계층은 없앨 수 있다 (error라는 내장함수를 상속하기 때문)
+    //하단 계층에 있는 것들을 바로 error 상속하도록
     this.name = 'BadRequestError';
 
     if (Error.captureStackTrace) {
