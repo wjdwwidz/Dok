f96b53afbaa90b1a3f7e06f88cec6fae31e0f21f
BangJunha <tkdlqjqkd@gmail.com>
2023-11-25 02:53:16 +0900

    Feat: add certification search feature

--- src/services/certificationPostService.js
+++ src/services/certificationPostService.js
@@ -1,33 +1,37 @@
 const CertificationPost = require('../models/certificationPost/certificationPost');
-const CertificationPostReview = require('../models/certificationPostReview/certificationPostReview');
 
 class CertificationPostService {
-  // 모든 인증글 조회
+  // 전체 인증글 조회
   getCertificationPosts() {
     const findCertificationPost = CertificationPost.find({})
       .populate('user')
       .populate('matchingPost')
-      .sort({ deletedAt: null })
       .sort({ createdAt: -1 }); // 인증글을 createdAt 기준으로 내림차순으로 정렬
+    const getCertificationPosts = findCertificationPost.find({
+      deletedAt: null,
+    });
 
-    return findCertificationPost;
+    return getCertificationPosts;
   }
 
   // 상세 인증글 조회
-  getCertificationPostDetail() {
-    const { userId } = req.params;
+  getCertificationPostDetail(postId) {
     const findCertificationPostDetail = CertificationPost.find({
-      _id: userId,
+      _id: postId,
     }).populate('review');
 
     return findCertificationPostDetail;
   }
 
   // 인증글 생성 (아직 이미지 받아오는 방식이 정해지지 않아서 certificationImg는 작성x)
-  postCertificationPost() {
-    const { user, matchingPost, sublocation, postText, review, deletedAt } =
-      req.body;
-
+  postCertificationPost(
+    user,
+    matchingPost,
+    sublocation,
+    postText,
+    review,
+    deletedAt,
+  ) {
     const newCertificationPost = CertificationPost.create({
       user,
       matchingPost,
@@ -41,55 +45,44 @@ class CertificationPostService {
   }
 
   // 인증글 삭제
-  deleteCertificationPost() {
-    const { certificationPostId } = req.body;
-    const removePost = CertificationPost.updateOne(
-      {
-        _id: certificationPostId,
-      },
-      {
-        deletedAt: Date.now(),
-      },
-    );
+  // deleteCertificationPost(certificationPostId) {
+  //   const removePost = CertificationPost.updateOne(
+  //     {
+  //       _id: certificationPostId,
+  //     },
+  //     {
+  //       deletedAt: Date.now(),
+  //     },
+  //   );
 
-    return removePost;
-  }
+  //   return removePost;
+  // }
 
-  // req.params에서 _id값을 받아서 그 값이 일치하는 인증글의 review가 처음에는 null값이니깐 그 값을 update해줘야한다.
   // 리뷰 생성
-  postCertificationPostReview() {
-    const { certificationPostId } = req.params;
+  postCertificationPostReview(certificationPostId, reviewText, score) {
     const newReview = CertificationPost.updateOne(
       {
         _id: certificationPostId,
       },
       {
         reviewText,
+        score,
       },
     );
-    // const { reviewText } = req.body.review;
-    // const { name } = req.body.user;
-    // const newReview = CertificationPost.find({ _id: postId }).create({
-    //   name,
-    //   reviewText,
-    //   // 별점(score) 추가 예정
-    // });
 
     return newReview;
   }
 
   // 리뷰 수정
   // 생성 과정과 동일
-  putCertificationPostReview() {
-    const { certificationPostId } = req.params;
-    const { reviewText } = req.body.review;
-
+  putCertificationPostReview(certificationPostId, reviewText) {
     const updatedReview = CertificationPost.updateOne(
       {
         _id: certificationPostId,
       },
       {
         reviewText,
+        score,
       },
     );
 
@@ -106,17 +99,40 @@ class CertificationPostService {
     })
       .populate('user')
       .populate('matchingPost')
-      .populate('review')
-      .sort({ deletedAt: null });
+      .populate('review');
+    const getCertificationPosts = locationCertificationPost.find({
+      deletedAt: null,
+    });
 
-    return locationCertificationPost;
+    return getCertificationPosts;
   }
+
   // 날짜 선택
-  // certificationPost의 createdAt 데이터에서 년/월/일 만 find를 해줘야한다.
-  // 내 추측
-  // split("-") - 이렇게 년/월/일 분리 후, 반복문으로 각각 비교?
-  // 그런데 이렇게하면 req.body에서 받아온 createdAt 값도 분리하고 찾고자하는 날짜도 분리.. 각각 비교..? 더 고민 필요.
-  dateCertificationPost() {}
+  dateCertificationPost(createdAt) {
+    const dateCertificationPost = CertificationPost.find({
+      createdAt: createdAt,
+    })
+      .populate('user')
+      .populate('matchingPost');
+    const getCertificationPosts = dateCertificationPost.find({
+      deletedAt: null,
+    });
+
+    return getCertificationPosts;
+  }
+
+  // 오래된순 (기본은 최신순)
+  getCertificationPosts() {
+    const findCertificationPost = CertificationPost.find({})
+      .populate('user')
+      .populate('matchingPost')
+      .sort({ createdAt: 1 }); // 인증글을 createdAt 기준으로 오름차순으로 정렬
+    const getCertificationPosts = findCertificationPost.find({
+      deletedAt: null,
+    });
+
+    return getCertificationPosts;
+  }
 }
 
 module.exports = CertificationPostService;
