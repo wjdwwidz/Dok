0715882ccbcc079debb4156fbbeab2a592a680b0
wjdwwidz <wjdwwidz@gmail.com>
2023-11-25 01:27:15 +0900

    Feat: create MyInfoResponse class

---
+++ src/dtos/users/myInfoResponse.js
@@ -0,0 +1,16 @@
+class MyInfoResponse {
+  constructor(user, userDogs) {
+    this.userId = user.userId;
+    this.name = user.name;
+    this.nickname = user.nickname;
+    this.phoneNumber = user.phoneNumber;
+    this.address = user.address;
+    this.introduce = user.introduce;
+    this.isCertificated = user.isCertificated;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+    this.userDogs = userDogs;
+  }
+}
+
+module.exports = MyInfoResponse;
