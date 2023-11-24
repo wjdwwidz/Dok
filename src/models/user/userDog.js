8c04ce7b60cb8892092af9f996ac1c2ba4dd2b87
wjdwwidz <wjdwwidz@gmail.com>
2023-11-25 01:26:04 +0900

    Feat: create UserDog model

---
+++ src/models/user/userDog.js
@@ -0,0 +1,46 @@
+const { model, Schema } = require('mongoose');
+
+const userDogSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  dogName: {
+    type: String,
+    required: true,
+  },
+  dogImg: {
+    type: String,
+    required: false,
+  },
+  birth: {
+    type: String,
+    required: true,
+  },
+  dogType: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  personality: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: String,
+    required: false,
+  },
+  deletedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+userDogSchema.set('timestamps', true);
+const UserDog = model('UserDog', userDogSchema);
+
+module.exports = UserDog;
