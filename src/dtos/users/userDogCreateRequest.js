12490f781f23425e40ff8e080298bc44fb981648
wjdwwidz <wjdwwidz@gmail.com>
2023-11-28 18:29:26 +0900

    Feat: add userDogCreateRequest

--- src/dtos/users/userDogCreateRequest.js
+++ src/dtos/users/userDogCreateRequest.js
@@ -1,3 +1,43 @@
-class UserDogCreateRequest {}
+//const { BadRequestError } = require('../../errors/badReqestError');
+class UserDogCreateRequest {
+  constructor(user, dogName, dogImg, birth, dogType, gender, personality) {
+    this.validate(user, dogName, dogImg, birth, dogType, gender, personality);
+
+    this.user = user;
+    this.dogName = dogName;
+    this.dogImg = dogImg;
+    this.birth = birth;
+    this.dogType = dogType;
+    this.gender = gender;
+    this.personality = personality;
+  }
+  getUserId() {
+    return this.user;
+  }
+
+  getDogName() {
+    return this.dogName;
+  }
+
+  getDogImg() {
+    return this.dogImg;
+  }
+
+  getBirth() {
+    return this.birth;
+  }
+
+  getDogType() {
+    return this.dogType;
+  }
+
+  getGender() {
+    return this.gender;
+  }
+
+  getPersonality() {
+    return this.personality;
+  }
+}
 
 module.exports = UserDogCreateRequest;
