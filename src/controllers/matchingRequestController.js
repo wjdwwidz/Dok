9d57546d97a9ae339bef02eb7859826bce144906
BangJunha <tkdlqjqkd@gmail.com>
2023-11-28 19:13:38 +0900

    Feat: fix matchingRequest feature final

--- src/controllers/matchingRequestController.js
+++ src/controllers/matchingRequestController.js
@@ -1,42 +1,25 @@
 const MatchingRequestService = require('../services/matchingRequestService');
 
-// 강아지 정보 받아오기
-const getDogInfo = async (req, res, next) => {
-  try {
-    const { userId } = req.params;
-    const getUserDog = await MatchingRequestService.getUserDogInfo(userId);
-
-    res.status(200).json(getUserDog);
-  } catch (err) {
-    next(err);
-  }
-};
-
-// 매칭글 신청하기
-const postMatchingRequest = async (req, res, next) => {
+// real
+const matchingRequest = async (req, res, next) => {
   try {
     const { userId } = req.params;
     const {
-      userDog,
       price,
       location,
       locationDetail,
       walkingDate,
       walkingDuration,
       requestText,
-      deletedAt,
     } = req.body;
-
     const newMatchingRequest = await MatchingRequestService.postMatchingRequest(
       userId,
-      userDog,
       price,
       location,
       locationDetail,
       walkingDate,
       walkingDuration,
       requestText,
-      deletedAt,
     );
 
     res.status(200).json(newMatchingRequest);
@@ -45,4 +28,49 @@ const postMatchingRequest = async (req, res, next) => {
   }
 };
 
-module.exports = { getDogInfo, postMatchingRequest };
+// // 강아지 정보 받아오기
+// const getDogInfo = async (req, res, next) => {
+//   try {
+//     const { userId } = req.params;
+//     const getUserDog = await MatchingRequestService.getUserDogInfo(userId);
+
+//     res.status(200).json(getUserDog);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// // 매칭글 신청하기
+// const postMatchingRequest = async (req, res, next) => {
+//   try {
+//     const { userId } = req.params;
+//     const {
+//       userDog,
+//       price,
+//       location,
+//       locationDetail,
+//       walkingDate,
+//       walkingDuration,
+//       requestText,
+//       deletedAt,
+//     } = req.body;
+
+//     const newMatchingRequest = await MatchingRequestService.postMatchingRequest(
+//       userId,
+//       userDog,
+//       price,
+//       location,
+//       locationDetail,
+//       walkingDate,
+//       walkingDuration,
+//       requestText,
+//       deletedAt,
+//     );
+
+//     res.status(200).json(newMatchingRequest);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+module.exports = { matchingRequest };
--- src/routers/matchingRequestRouter.js
+++ src/routers/matchingRequestRouter.js
@@ -1,14 +1,12 @@
 const { Router } = require('express');
-const {
-  getDogInfo,
-  postMatchingRequest,
-} = require('../controllers/matchingRequestController');
+const { matchingRequest } = require('../controllers/matchingRequestController');
 
 const router = Router();
 
 // 강아지 정보 받아오기
-router.get('/doginformation/:userId', getDogInfo);
-// 매칭 신청하기 -> 수정필요
-router.post('/matchingRequest/:userId', postMatchingRequest);
+// router.get('/doginformation/:userId', getDogInfo);
+
+// 매칭 신청하기
+router.post('/matchingRequest/:userId', matchingRequest);
 
 module.exports = router;
