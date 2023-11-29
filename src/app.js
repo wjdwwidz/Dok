43d799ea58d6de73841e6e12432874630941ccea
wjdwwidz <wjdwwidz@gmail.com>
2023-11-30 01:37:14 +0900

    Feat: Add cors option

--- src/app.js
+++ src/app.js
@@ -10,12 +10,15 @@ require('dotenv').config();
 const app = express();
 
 const userRouter = require('./routers/userRouter');
+
+require('dotenv').config();
+
+const dbFill = require('./dbFill.js');
+
 const matchingPostRouter = require('./routers/matchingPostRouter.js');
 const certificationPostRouter = require('./routers/certificationRouter');
 const errorHandler = require('./middlewares/errorHandler');
 const matchingRequestRouter = require('./routers/matchingRequestRouter.js');
-const mainRouter = require('./routers/mainRouter.js');
-const myPageRouter = require('./routers/myPageRouter.js');
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
@@ -35,15 +38,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello express !');
 });
 
-app.use('/api/main', mainRouter);
-
 app.use('/api/users', userRouter);
-app.use('/api/mypage', myPageRouter); //마이페이지
-
 app.use('/api/matchingPostLists', matchingPostRouter); // 전체 게시글 불러오기
 app.use('/api/matchingPostDetail', matchingPostRouter); // 상세 정보 불러오기 ()
 
@@ -52,5 +51,6 @@ app.use('/api/certificationRouter', certificationPostRouter); // 인증글
 
 app.use(errorHandler);
 app.listen(process.env.PORT, () => {
+  // dbFill();
   console.log(`Express server starting on port ${process.env.PORT}`);
 });
