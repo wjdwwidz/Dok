293ce04fdc93f8708d8a9ed0c32e439e95fdfae6
wjdwwidz <wjdwwidz@gmail.com>
2023-11-30 22:00:45 +0900

    Feat: add post function

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,20 +1,21 @@
 //AWS S3 관련 설정 및 파일 업로드
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const { fromIni } = require('@aws-sdk/credential-provider-ini');
-env = require('dotenv');
-env.config();
+const { fromEnv } = require('@aws-sdk/credential-provider-ini');
+dotenv = require('dotenv');
+dotenv.config();
 
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const credentials = fromIni({
-  configFilepath:
-    process.env.AWS_SHARED_CREDENTIALS_FILE || '~/.aws/credentials',
+//const credentials = fromEnv();
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: fromEnv,
 });
 
-const s3Client = new S3Client({ region: process.env.AWS_REGION, credentials });
 const upload = multer({
   storage: multerS3({
-    client: s3Client,
+    s3: s3Client,
     bucket: 'dokawsbucket',
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
@@ -23,4 +24,5 @@ const upload = multer({
     },
   }),
 });
+
 module.exports = upload;
